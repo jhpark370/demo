77c5473e312d7fc2a1093e029c2e4b55fafda0d1
jhpark370 <jhpark370@hotmail.com>
2020-11-13 17:55:38 +0900

    JPA를 이용한 페이지 처리 및 검색 - 페이지 이동, 검색 조건 작성

--- src/main/java/kr/taxcube/myhome/controller/BoardController.java
+++ src/main/java/kr/taxcube/myhome/controller/BoardController.java
@@ -4,6 +4,10 @@ import kr.taxcube.myhome.model.Board;
 import kr.taxcube.myhome.repository.BoardRepository;
 import kr.taxcube.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,9 +27,47 @@ public class BoardController {
     @Autowired
     private BoardValidator boardValidator;
 
+
+//    public String list(Model model) {
+//        List<Board> boards = boardRepository.findAll();
+//        model.addAttribute("boards",boards);
+//        return"board/list";
+//}
+//    기본값을 2로해서 테스트해본다.
+//    @GetMapping("/list")
+//    public String list(Model model, @PageableDefault(size = 2) Pageable pageable, String searchText) {
+////        한페이지에 20개씩 보여주겠다.
+////        Page<Board> boards = boardRepository.findAll(PageRequest.of(0, 20));
+////        전체 데이터 갯수
+////        boards.getTotalElements();
+//        Page<Board> boards = boardRepository.findAll(pageable);
+////        시작페이지와 마지막 페이지 정의
+//        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+//        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+////        현재페이지 비활성화를 위해 리스트 페이지에서 아래 코드 삽입
+////        boards.getPageable().getPageNumber();
+//        model.addAttribute("startPage", startPage);
+//        model.addAttribute("endPage", endPage);
+//        model.addAttribute("boards", boards);
+//        return "board/list";
+//    }
+
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+//        한페이지에 20개씩 보여주겠다.
+//        Page<Board> boards = boardRepository.findAll(PageRequest.of(0, 20));
+//        전체 데이터 갯수
+//        boards.getTotalElements();
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+//        시작페이지와 마지막 페이지 정의
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+//        현재페이지 비활성화를 위해 리스트 페이지에서 아래 코드 삽입
+//        boards.getPageable().getPageNumber();
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
--- src/main/java/kr/taxcube/myhome/repository/BoardRepository.java
+++ src/main/java/kr/taxcube/myhome/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package kr.taxcube.myhome.repository;
 
 import kr.taxcube.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -15,5 +17,5 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 //    4.5.1 XML configuration - Using filters (Example 61, 63, 64 )
 //    List<Board> findByTitleAndContent(String title, String content);
 
-
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
